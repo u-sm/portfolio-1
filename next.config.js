4c83f29b68ab9aae4d8d999e10fd679b9e1ccb7c
Usman <138681525+u-sm@users.noreply.github.com>
2025-04-25 02:59:36 +0800

    fix: Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   output: 'export',          // Next.js 14+ static export
   trailingSlash: true       // ensures proper routing
-  //basePath: '/your-repo',    // if you’re serving from a sub-folder
+  basePath: '/portfolio-1',    // if you’re serving from a sub-folder
+  assetPrefix: '/portfolio-1',   // same as basePath
 }
