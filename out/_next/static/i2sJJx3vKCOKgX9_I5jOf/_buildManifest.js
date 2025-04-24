a5939b995db0ded443bdb3a956faefe73a5446d3
Usman <138681525+u-sm@users.noreply.github.com>
2025-04-25 03:39:20 +0800

    fix: other code

---
+++ out/_next/static/i2sJJx3vKCOKgX9_I5jOf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/845-69a0f6680a685e96.js","static/chunks/pages/index-08818ac8a2cefa8b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
