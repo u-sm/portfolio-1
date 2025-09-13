f7a7fa71cfaf1cb9c0a991159e4dd8c4782104f2
u-sm <u-sm@users.noreply.github.com>
2025-09-13 15:52:51 +0000

    deploy: 7c79446639d3340ce560a256a0dd630b53772c57

---
+++ _next/static/k64V2h8_uJf55fEqybHF4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/871-a87c6e50503ccbb2.js","static/chunks/pages/index-4ebd671a9399bf1e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
